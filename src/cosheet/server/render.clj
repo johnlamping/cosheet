761d1fa98fef80b10f1382319fe78a652252d426
John Lamping <cosheet@johnlamping.com>
2015-05-25 17:23:54 -0700

    Check in render.clj

---
+++ src/cosheet/server/render.clj
@@ -0,0 +1,277 @@
+(ns cosheet.server.render
+  (:require (cosheet [entity :as entity]
+                     [reporters
+                      :refer [value expr expr-let expr-seq]])))
+
+;;; Code to create hiccup style dom for a database entity.
+;;; Sub-components of the dom are represented as specified as
+;;;   [:component {:sibling-key sibling-key
+;;;                :definition definition
+;;;                :attributes <attributes to add>}]
+;;; as expected by dom_tracker.
+
+;;; For convenience here, style attributes are represented with their
+;;; own map, rather than as a string, so they are easier to adjust.
+;;; They must be turned into a string when the dom is sent to reagent.
+
+;;; For a basic entity, we show its contents and its user visible
+;;; elements, but not its non-user visible elements. The latter are
+;;; identified by, themselves, having elements with keyword contents.
+;;; in addition, an entity may be marked as a tag, by having
+;;; an element whose content is 'tag. The 'tag mark is considered
+;;; visible, because it changes the visible appearance of its element,
+;;; even though it is not shown in the usual way.
+;;; So, for example, the entity:
+;;;    ("Joe"
+;;;        "married"
+;;;        (39
+;;;           ("age" 'tag)
+;;;           "doubtful"))
+;;; would look like:
+;;;   Joe
+;;;     married
+;;;     age: 39
+;;;            doubtful
+
+(defn multiset-conj
+  "Add an item to a multiset,
+   represented as a map from items to multiplicities."
+  [ms item]
+  (update-in ms [item] #((fnil + 0) % 1)))
+
+(defn multiset
+  "Turn a seq into a multiset."
+  [items]
+  (reduce multiset-conj {} items))
+
+(defn visible-entity?
+  "Return true if an entity is visible to the user
+  (Doesn't have a keyword element.)"
+  [entity]
+  (expr-let [elements (entity/elements entity)
+             element-contents (expr-seq map entity/content elements)]
+    (not-any? keyword? element-contents)))
+
+(defn tag-specifier?
+  "Return true if an element is a specifier that the item pertains to is a tag.
+   (Note that a tag specifier is considered visible, but its presence affects
+   the user visible structure, even though it is not shown as its own cell."
+  [element]
+  (expr-let [content (entity/content element)]
+    (= content 'tag)))
+
+(defn filtered-elements
+  "Return a seq of all elements of the entity that satisfy the condition."
+  [entity condition]
+  (expr-let [elements (entity/elements entity)
+             passed (expr-seq map #(expr-let [passes (condition %)]
+                                        (when passes %))
+                                 elements)]
+    (filter identity passed)))
+
+(defn visible-elements
+  "Return the elements of an entity that are visible to the user."
+  [entity]
+  (filtered-elements entity visible-entity?))
+
+(defn tag-specifiers
+  "Return the tag specifiers of an entity."
+  [entity]
+  (filtered-elements entity tag-specifier?))
+
+(defn canonical-info
+  "Given an item, return a canonical representation of its
+   information, ignoring all information not directly visible
+  to the user."
+  ;; We record the elements as a map from element to multiplicities,
+  ;; so that we are not sensitive to the order of the elements.
+  ;; That is easier than sorting, because Clojure doesn't define
+  ;; a sort order between heterogenous types, like strings and ints.
+  [entity]
+  (if (entity/atom? entity)
+    (entity/content entity)
+    (expr-let [content (entity/content entity)
+               elements (visible-elements entity)]
+      (expr-let [content-info (canonical-info content)
+                 element-infos (expr-seq map canonical-info elements)]
+        (if (empty? element-infos)
+          content-info
+          [content-info (multiset element-infos)])))))
+
+(defn canonical-info-set
+  "Given a seq of items, return a canonical representation of the items,
+   treated as a multi-set."
+  [entities]
+  (expr-let [canonicals (expr-seq map canonical-info entities)]
+    (multiset canonicals)))
+
+(defn order-items
+  "Return the items in the proper sort order."
+  [items]
+  (expr-let [order-info
+             (expr-seq map #(entity/label->content % :order) items)]
+    (map second (sort (map vector order-info items)))))
+
+(defn into-attributes
+  "Add attributes to an attribute map,
+   correctly handling multiple classes or styles."
+  [accumulator attributes]
+  (reduce (fn [accumulator [key value]]
+            (update-in accumulator [key]
+                       (fn [current]
+                         (if current
+                           (case key
+                             :class (str current " " value)
+                             :style (into current value))
+                           value))))
+          accumulator attributes))
+
+(defn dom-attributes
+  "Return the current specified attributes of a dom."
+  [dom]
+   (let [[dom-tag second & remainder] dom]
+     (if  (= dom-tag :component)
+       second
+       (if (map? second)
+         second
+         {}))))
+
+(defn add-attributes
+  "Add attributes to a hiccup dom descriptor"
+  [dom attributes]
+  (let [[dom-tag second & remainder] dom]
+    (if (= dom-tag :component)
+      (do (assert (empty? remainder))
+          [:component
+           (update-in second [:attributes] #(into-attributes % attributes))])
+      (let [[attr remainder] (if (map? second)
+                               [second remainder]
+                               [{} (rest dom)])]
+        (apply vector
+               dom-tag
+               (into-attributes attr attributes)
+               remainder)))))
+
+(defn stack-vertical
+  "Make a dom stack vertically with its siblings."
+  [dom]
+  (let [style (:style (dom-attributes dom))
+        display (:display style)
+        width (:width style)]
+    (add-attributes (if width dom (add-attributes dom {:style {:width "100%"}}))
+                    {:style {:display
+                             (case display
+                               (nil "block" "inline-block") "block"
+                               ("table" "inline-table") "table") }})))
+
+(def item-DOM)
+
+(defn make-component
+  "Make a component dom descriptor, with the given sibling key and definition,
+   and, optionally, additional attributes."
+  ([sibling-key definition]
+   [:component {:sibling-key sibling-key
+                :definition definition}])
+  ([sibling-key definition attributes]
+   (assert (map? attributes))
+   [:component {:sibling-key sibling-key
+                :definition definition
+                :attributes attributes}]))
+
+(defn group-by-tag
+  "Given a sequence of items, group consequtively, identically tagged,
+   items together. Return a list of the groups, where each group is
+   described as a list of [item, tags] pairs."
+  [items]
+  (expr-let [tag-lists (expr-seq
+                        map #(entity/label->elements % 'tag) items)
+             canonical-tags (expr-seq map canonical-info-set tag-lists)]
+    (first (reduce (fn [[groups prev-item-canonical-tags]
+                        [item item-tags item-canonical-tags]]
+                     [(if (= item-canonical-tags prev-item-canonical-tags)
+                        (update-in groups [(dec (count groups))]
+                                   #(conj % [item item-tags]))
+                        (conj groups [[item item-tags]]))
+                      item-canonical-tags])
+                   [[] ::none]
+                   (map vector items tag-lists canonical-tags)))))
+
+(defn vertical-stack
+  "If there is only one item in the doms, return it. Otherwise, return
+  a vertical stack of the items."
+  [doms]
+  (case (count doms)
+    0 [:div]
+    1 (first doms)
+    (apply vector :div
+           (map stack-vertical doms))))
+
+;;; TODO: Make the tag styling fancier, with things like rounded
+;;; corners and graduated colors.
+(def tag-styling {:style {:color "#66FFFF"}})
+
+(defn tag-component
+  "Return the component for a tag element."
+  [element inherited]
+  (expr-let [tag-specs (tag-specifiers element)]
+    (make-component
+     element
+     [item-DOM element (set tag-specs) inherited]
+     {})))
+
+(defn tags-DOM
+  "Given a sequence of tags, return components for the given items, wrapped in
+   a div if there is more than one."
+  [tags inherited]
+  (expr-let [tag-components (expr-seq map #(tag-component % inherited) tags)]
+    (add-attributes (vertical-stack tag-components) tag-styling)))
+
+(defn tag-items-pair-DOM
+  "Given a list of items and the tags for each, where the tags for each item
+  are equivalent, generate DOM for the items, labeled with the tags."
+  ;;; TODO: Add some data-* attributes to the tag items that indicate
+  ;;; what items they pertain to.
+  ;;; TODO: pass down, via inherited, how deeply nested the item is,
+  ;;; and for deep items, use a layout with tag above content to conserve
+  ;;; horizontal space.
+  [items-and-tags inherited]
+  (expr-let [item-doms (expr-seq
+                        map (fn [[item tag-list]]
+                              (make-component
+                               item [item-DOM item (set tag-list) inherited]))
+                        items-and-tags)
+             tags-dom (expr tags-DOM
+                        (order-items (get-in items-and-tags [0 1]))
+                        inherited)]
+    [:div {:style {:display "table-row"}}
+     (add-attributes tags-dom
+                     {:style {:display "table-cell" :width "30%"}})
+     (add-attributes (vertical-stack item-doms)
+                     {:style {:display "table-cell" :width "70%"}})]))
+
+(defn tagged-items-DOM
+  "Return DOM for the given items, as a grid of tags and values."
+  [items inherited]
+  (expr-let [rows (expr group-by-tag (order-items items))
+             row-doms (expr-seq map #(tag-items-pair-DOM % inherited) rows)]
+    (apply vector :div {:style {:display "table"}} row-doms)))
+
+(defn item-DOM
+  "Return a hiccup representation of DOM describing
+  an item and all its elements. Where appropriate, inherit
+  properties from the map of inherited properties."
+  [item excluded inherited]
+  ;;; TODO: pass down, via inherited, how deeply nested the item is,
+  ;;; and for deep items, use a layout with tag above content to conserve
+  ;;; horizontal space.
+  (expr-let [content (entity/content item)
+             elements (visible-elements item)]
+    (let [elements (remove excluded elements)
+          content-dom
+          (if (entity/atom? content)
+            [:div (if (= content :none) "" (str content))]
+            (make-component "content" [item-DOM content #{} inherited]))]
+      (if (empty? elements)
+          content-dom
+          (expr-let [elements-dom (tagged-items-DOM elements inherited)]
+            (vertical-stack [content-dom elements-dom]))))))
---
+++ test/cosheet/server/render_test.clj
@@ -0,0 +1,173 @@
+(ns cosheet.server.render-test
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.data :refer [diff]]
+            [clojure.pprint :refer [pprint]]
+            (cosheet
+             [store :refer :all]
+             [store-utils :refer :all]
+             [entity :as entity  :refer [to-list description->entity]]
+             [reporters :refer [expr expr-let expr-seq]]
+             [debug :refer [current-value envs-to-list
+                            let-propagated let-propagated-store]]
+             entity-impl
+             store-impl
+             mutable-store-impl)
+            [cosheet.server.render :refer :all]
+            ; :reload
+            ))
+
+(defn canonicalize
+  "Turn a list form of an entity into a canonical representation,
+   that is insensitive to the order of elements. (We record the elements in
+   a map from entity to multiplicity.)"
+  [entity]
+  (if (sequential? entity)
+    [(first entity)
+     (reduce (fn [ms entity]
+               (update-in ms [(canonicalize entity)] #((fnil + 0) % 1)))
+             {} (rest entity))]
+    entity))
+
+(deftest render-test
+  (let [jane '("Jane" (1 :order) "plain" "plain")
+        joe '("Joe"
+              (2 :order)
+              ("male" (1 :order))
+              ("married" (2 :order))
+              (39 (3 :order)
+                  ("age" tag)
+                  ("doubtful" "confidence")))]
+    (is (= (set (map canonicalize
+                     (let-propagated [him joe]
+                       (expr-seq map to-list (visible-elements him)))))
+           (set (map canonicalize (rest (rest joe))))))
+    (is (= (map canonicalize
+                (let-propagated [test '("age" tag "not-tag")]
+                  (expr-seq map to-list (tag-specifiers test))))
+           ['tag]))
+    (is (= (let-propagated [him joe, her jane]
+             (canonical-info-set [him her]))
+           {["Joe"
+             {"married" 1
+              "male" 1
+              [39 {["age" {'tag 1}] 1, ["doubtful" {"confidence" 1}] 1}] 1}] 1
+            ["Jane" {"plain" 2}] 1}))
+    (is (= (map canonicalize
+                (let-propagated [him joe, her jane]
+                  (expr-seq map to-list (order-items [him her]))))
+           (map canonicalize [jane joe])))
+    (is (= (-> {}
+               (into-attributes {:class "foo"
+                                 :style {:font "bold"}
+                                 :other "abc"})
+               (into-attributes {:class "bar"
+                                 :style {:text "large"}
+                                 :misc "junk"})
+               (into-attributes {:class "baz"
+                                 :style {:color "red"}}))
+           {:class "foo bar baz"
+            :style {:font "bold" :text "large" :color "red"}
+            :other "abc"
+            :misc "junk"}))
+    (is (= (-> [:component {:definition [:foo :bar]}]
+               (add-attributes {:class "foo"
+                                :style {:font "bold"}
+                                :other "abc"})
+               (add-attributes {:class "bar"
+                                :style {:text "large"}
+                                :misc "junk"}))
+           [:component {:definition [:foo :bar]
+                        :attributes {:class "foo bar"
+                                     :style {:font "bold" :text "large"}
+                                     :other "abc"
+                                     :misc "junk"}}]))
+    (is (= (-> [:div "hi there"]
+               (add-attributes {:class "foo"
+                                :style {:font "bold"}
+                                :other "abc"})
+               (add-attributes {:class "bar"
+                                :style {:text "large"}
+                                :misc "junk"}))
+           [:div {:class "foo bar"
+                  :style {:font "bold" :text "large"}
+                  :other "abc"
+                  :misc "junk"}
+            "hi there"]))
+    (let-propagated [him joe]
+      (expr-let [elements (visible-elements him)
+                 groups (group-by-tag elements)]
+        (is (= (count groups) 2))
+        (is (= (set (map count groups)) #{1 2}))
+        (is (= (apply + (map #(count (second (first %))) groups)) 1))))
+    (is (= (tags-DOM [] {}) [:div {:style {:color "#66FFFF"}}]))
+    (let [[dom fred fred-tag]
+          (let-propagated [fred '("Fred" tag)]
+            (expr-let [dom (tags-DOM [fred] {})
+                       fred-elements (entity/elements fred)]
+              [dom fred (first fred-elements)]))]
+      (is (= dom 
+             [:component {:sibling-key fred
+                          :definition [item-DOM fred #{fred-tag} {}]
+                          :attributes {:style {:color "#66FFFF"}}}])))
+    (let [[dom fred fran]
+          (let-propagated [fred '("Fred" tag)
+                           fran "Fran"]
+            (expr-let [dom (tags-DOM [fred fran] {})]
+              [dom fred fran]))
+          fred-tag (first (current-value (entity/elements fred)))]
+      (is (= dom 
+             [:div
+              {:style {:color "#66FFFF"}}
+              [:component {:sibling-key fred
+                           :definition [item-DOM fred #{fred-tag} {}]
+                           :attributes
+                           {:style {:display "block"
+                                    :width "100%"}}}]
+              [:component {:sibling-key fran
+                           :definition [item-DOM fran #{} {}]
+                           :attributes
+                           {:style  {:display "block"
+                                     :width "100%"}}}]]
+             )))
+    (is (= (let-propagated [fred "Fred"]
+             (item-DOM fred #{} {}))
+           [:div "Fred"]))
+    (let [[dom joe]
+          (let-propagated [him joe]
+            (expr-let [dom (item-DOM him #{} {})]
+              [dom him]))
+          male (first (current-value (entity/label->elements joe 1)))
+          married (first (current-value (entity/label->elements joe 2)))
+          age (first (current-value (entity/label->elements joe "age")))
+          age-tag (first (current-value (entity/label->elements age 'tag)))
+          age-tag-spec (first (current-value (entity/elements age-tag)))]
+      (is (= dom
+             [:div
+              [:div {:style {:width "100%" :display "block"}} "Joe"]
+              [:div {:style {:width "100%" :display "table"}}
+               [:div {:style {:display "table-row"}}
+                [:div {:style {:color "#66FFFF"
+                               :width "30%"
+                               :display "table-cell"}}]
+                [:div {:style {:width "70%" :display "table-cell"}}
+                 [:component {:attributes {:style {:width "100%"
+                                                   :display "block"}}
+                              :sibling-key male
+                              :definition [item-DOM male #{} {}]}]
+                 [:component {:attributes {:style {:width "100%"
+                                                   :display "block"}}
+                              :sibling-key married
+                              :definition [item-DOM married #{} {}]}]]]
+               [:div {:style {:display "table-row"}}
+                [:component {:attributes {:style {:color "#66FFFF"
+                                                  :width "30%"
+                                                  :display "table-cell"}}
+                             :sibling-key age-tag
+                             :definition [item-DOM age-tag #{age-tag-spec} {}]}]
+                [:component {:attributes {:style {:width "70%"
+                                                  :display "table-cell"}}
+                             :sibling-key age
+                             :definition [item-DOM age #{age-tag} {}]}]]]]))
+      )))
+
+

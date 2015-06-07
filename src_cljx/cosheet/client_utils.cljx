4d6756a7794683908d87c3421aa753121a6df152
John Lamping <cosheet@johnlamping.com>
2015-06-06 18:44:57 -0700

    client_utils handles subcomponent attributes in the client dom.

--- src_cljx/cosheet/client_utils.cljx
+++ src_cljx/cosheet/client_utils.cljx
@@ -1,11 +1,15 @@
 (ns cosheet.client-utils
-  #+cljs (:require [reagent.core :as reagent :refer [atom]])
+  (:require #+cljs [reagent.core :as reagent :refer [atom]]
+            [cosheet.dom-utils :refer [into-attributes]])
   )
 
 (def components (clojure.core/atom {}))
 
-(defn component [name]
-  @(@components name))
+(defn component [attributes name]
+  (let [[tag dom-attributes & rest] @(@components name)]
+    (into
+     [tag (into-attributes (dissoc dom-attributes :version) attributes)]
+     rest)))
 
 (defn set-difference
   "Implement set difference ourselves, because I can't figure out how to
@@ -32,7 +36,7 @@
   [dom]
   (if (vector? dom)
     (if (= (first dom) component)
-      [(second dom)]
+      [(nth dom 2)]
       (reduce (fn [subcomponents dom]
                 (into subcomponents (subcomponent-ids dom)))
               [] dom))
@@ -47,7 +51,7 @@
 (defn add-ids
   "Add atoms for the given ids to the map."
   [map ids]
-  (reduce (fn [map id] (assoc map id (atom [:div {:id id :data-version -1}])))
+  (reduce (fn [map id] (assoc map id (atom [:div {:id id :version -1}])))
           map ids))
 
 (defn into-atom-map
@@ -59,11 +63,11 @@
    (fn [amap]
      (reduce
       (fn [amap dom]
-        (let [{:keys [id data-version]} (second dom)]
+        (let [{:keys [id version]} (second dom)]
           (if (contains? amap id)
             (let [old-dom @(amap id)
-                  old-version (:data-version (second old-dom))]
-              (if (> data-version old-version)
+                  old-version (:version (second old-dom))]
+              (if (> version old-version)
                 (let [old-subcomponents (set (subcomponent-ids old-dom))
                       subcomponents (set (subcomponent-ids dom))]
                   (do (reset! (amap id) dom)
--- test/cosheet/client_utils_test.clj
+++ test/cosheet/client_utils_test.clj
@@ -6,6 +6,23 @@
             ; :reload
             ))
 
+(deftest component-test
+  (reset! components {:a1 (atom [:div
+                                 {:id :a1
+                                  :version 2
+                                  :class "c"
+                                  :style {:bar "bar"}}
+                                 1])})
+  (is (= (component {} :a1)
+         [:div {:id :a1 :class "c" :style {:bar "bar"}} 1]))
+  (is (= (component {:style {:foo "foo"} :width 5 :class "b"} :a1)
+         [:div
+          {:id :a1
+           :style {:bar "bar" :foo "foo"}
+           :width 5
+           :class "c b"}
+          1])))
+
 (deftest set-difference-test
   (is (= (set-difference #{1 2} #{2 3}) #{1})))
 
@@ -18,44 +35,44 @@
 
 (deftest subcomponent-ids-test
   (is (= (set (subcomponent-ids [:div
-                                 [component :message]
-                                 [component :clock]]))
+                                 [component {} :message]
+                                 [component {} :clock]]))
          #{:message :clock})))
 
 (deftest into-atom-map-test
-  (let [a1 (atom [:div {:id :a1 :data-version 1} 1])
-        a2 (atom [:div {:id :a2 :data-version 3} 2])
-        a3 (atom [:div {:id :a3 :data-version 1} 3])
+  (let [a1 (atom [:div {:id :a1 :version 1} 1])
+        a2 (atom [:div {:id :a2 :version 3} 2])
+        a3 (atom [:div {:id :a3 :version 1} 3])
         am (atom {:a1 a1
                   :a2 a2
                   :a3 a3})]
-    (into-atom-map am [[:div {:id :a1 :data-version 2} 11]
-                       [:div {:id :a2 :data-version 2} 22]
-                       [:div {:id :a4 :data-version 4} 44]])
+    (into-atom-map am [[:div {:id :a1 :version 2} 11]
+                       [:div {:id :a2 :version 2} 22]
+                       [:div {:id :a4 :version 4} 44]])
     (is (= (count @am) 3))
     (is (= (@am :a1) a1))
     (is (= (@am :a2) a2))
     (is (= (@am :a3) a3))
-    (is (= @a1 [:div {:id :a1 :data-version 2} 11]))
-    (is (= @a2 [:div {:id :a2 :data-version 3} 2]))
-    (is (= @a3 [:div {:id :a3 :data-version 1} 3])))
-  (let [am (atom {:root (atom [:div {:id :root :data-version 0}])})]
+    (is (= @a1 [:div {:id :a1 :version 2} 11]))
+    (is (= @a2 [:div {:id :a2 :version 3} 2]))
+    (is (= @a3 [:div {:id :a3 :version 1} 3])))
+  (let [am (atom {:root (atom [:div {:id :root :version 0}])})]
     (into-atom-map am [[:div
-                        {:id :root :data-version 1}
-                        [component :message]
-                        [component :clock]]])
+                        {:id :root :version 1}
+                        [component {} :message]
+                        [component {} :clock]]])
     (is (= (set (keys @am)) #{:root :message :clock}))
-    (into-atom-map am [[:div {:id :message :data-version 1} "hi"]
-                       [:div {:id :clock :data-version 1} "now"]
-                       [:div {:id :a4 :data-version 4} 44]])
+    (into-atom-map am [[:div {:id :message :version 1} "hi"]
+                       [:div {:id :clock :version 1} "now"]
+                       [:div {:id :a4 :version 4} 44]])
     (is (= (set (keys @am)) #{:root :message :clock}))
-    (= @(@am :message) [:div {:id :message :data-version 1} "hi"])
+    (= @(@am :message) [:div {:id :message :version 1} "hi"])
     (into-atom-map am [[:div
-                        {:id :root :data-version 2}
-                        [component :message]
-                        [component :tick]]
-                       [:div {:id :tick :data-version 2} "tock"]])
+                        {:id :root :version 2}
+                        [component {} :message]
+                        [component {} :tick]]
+                       [:div {:id :tick :version 2} "tock"]])
     (is (= (set (keys @am)) #{:root :message :tick}))
-    (= @(@am :tick) [:div {:id :message :data-version 2} "tock"]))
+    (= @(@am :tick) [:div {:id :message :version 2} "tock"]))
   )
 

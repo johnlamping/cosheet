2bc59e24f0c29cd3f4648a43c36ff39b6df42b02
John Lamping <cosheet@johnlamping.com>
2015-11-02 23:15:59 -0800

    mutable-store uses mutable-manager

--- src/cosheet/mutable_store_impl.clj
+++ src/cosheet/mutable_store_impl.clj
@@ -1,75 +1,15 @@
 (ns cosheet.mutable-store-impl
   (:require (cosheet [store :refer :all]
+                     [mutable-manager
+                      :refer [new-mutable-management
+                              get-or-make-reporter
+                              describe-and-swap-control-return!]]
                      [reporters :as reporter
                       :refer [set-value! set-manager!
                               attended? new-reporter invalid]]
                      [utils :refer [call-with-latest-value
                                     update-in-clean-up
                                     swap-control-return!]])))
-
-(defn inform-reporter
-  "Make sure the reporter has the latest value from the store."
-  [immutable-store reporter]
-  (let [[f & args] (:fetch (reporter/data reporter))]
-    (set-value! reporter (apply f immutable-store args))))
-
-(defn manager-callback
-  "Callback when a reporter starts or stops needing updates from the store.
-   If id is non-nil, then only changes to that item can affect the value."
-  [reporter store id]
-  (call-with-latest-value
-   #(attended? reporter)
-   (fn [attended]
-     (let [fetch (:fetch (reporter/data reporter))]
-       (if attended
-         (do
-           (swap! (:data store)
-                  (fn [data]
-                    (-> data
-                        (update-in [:subscriptions id]
-                                   #((fnil conj #{}) % reporter))
-                        (update-in [:fetch->attended-reporter fetch]
-                                   ;; Due to races, we might already
-                                   ;; have another reporter for this
-                                   ;; expression, in which case, leave it.
-                                   (fn [existing] (or existing reporter))))))
-           (inform-reporter (:state @(:data store)) reporter))
-         (do
-           (set-value! reporter invalid)
-           (swap! (:data store)
-                  (fn [data]
-                    (-> data
-                        (update-in-clean-up
-                         [:subscriptions id] #(disj % reporter))
-                        (update-in-clean-up
-                         [:fetch->attended-reporter fetch]
-                         ;; Due to races, we might already have
-                         ;; another reporter for this expression, in
-                         ;; which case, leave it.
-                         (fn [existing]
-                           (if (= existing reporter) nil existing))))))))))))
-
-(defn get-or-make-reporter
-  "Retrieve or create a reporter that gives the value of the function
-   applied to the current state of the store and the arguments. If id
-   is non-nil, the result must depend only on id and its elements."
-  [id fn store & args]
-  (let [fetch (cons fn args)]
-    (or (get-in @(:data store) [:fetch->attended-reporter fetch])
-        (new-reporter :manager [manager-callback store id]
-                      :fetch fetch))))
-
-(defn inform-reporters-for-key
-  "Update all reporters that depend on the given key."
-  [immutable-store subscriptions key]
-  (doseq [reporter (get subscriptions key)]
-    (inform-reporter immutable-store reporter)))
-
-(defn inform-reporters
-  "Update all reporters that care about the changes."
-  [new-store subscriptions keys]
-  (doseq [key keys]
-    (inform-reporters-for-key new-store subscriptions key)))
 
 (defn items-affected-by-id
   "Return a seq of items that might be affected by a change to the given id."
@@ -95,68 +35,50 @@
       "A store that contains an immutable store,
        supports mutation to that store,
        and returns reporter objects for queries."}
-  [;; A map holding the reporter's data, consisting of
-   data
-   
-   ;; An atom holding the current immutable state
-   ;; :state
-
-   ;; A mutable map from item id to a set of reporters that need
-   ;; to be checked on any change to that item or its elements.
-   ;; Reporters indexed under a nil id must be checked for all changes.
-   ;; :subscriptions
-
-   ;; A mutable map from fetch expression to an attended reporter with that
-   ;; fetch, if there is one.
-   ;; :fetch->attended-reporter
+  [management ;; A mutable-manager managemente holding the immutable
+              ;; store as its value. 
    ]
 
   Store
 
   (id-valid? [this id]
-    (get-or-make-reporter id id-valid? this id))
+    (get-or-make-reporter [id] id-valid? (:management this) id))
 
   (id-label->element-ids [this id label]
-    (get-or-make-reporter id id-label->element-ids this id label))
+    (get-or-make-reporter
+     [id] id-label->element-ids (:management this) id label))
 
   (id->element-ids [this id]
-    (get-or-make-reporter id id->element-ids this id))
+    (get-or-make-reporter [id] id->element-ids (:management this) id))
 
   (id->content [this id]
-    (get-or-make-reporter id id->content this id))
+    (get-or-make-reporter [id] id->content (:management this) id))
 
   (id->content-reference [this id]
-    (get-or-make-reporter id id->content-reference this id))
+    (get-or-make-reporter [id] id->content-reference (:management this) id))
 
   (candidate-matching-ids [this item]
-    (get-or-make-reporter nil candidate-matching-ids this item))
+    (get-or-make-reporter [nil] candidate-matching-ids (:management this) item))
 
   (mutable-store? [this] true)
   
   MutableStore
 
-  (current-store [this] (:state @(:data this)))
+  (current-store [this] (:value @(:management this)))
 
   (do-update! [this update-fn]
     (do-update-control-return! this (fn [store] [(update-fn store) nil])))
 
   (do-update-control-return! [this update-fn]
-    (let [[modified-ids subscriptions old-store new-store result]
-          (swap-control-return!
-           (:data this)
-           (fn [data]
-             (let [old-store (:state data)
-                   subscriptions (:subscriptions data)
-                   [updated-store result] (update-fn old-store)
-                   [new-store modified-ids] (fetch-and-clear-modified-ids
-                                             updated-store)]
-               [(assoc data :state new-store)
-                [modified-ids subscriptions old-store new-store result]])))]
-      (when (not= (count modified-ids) 0)
-        (inform-reporters
-         new-store subscriptions
-         (keys-affected-by-ids modified-ids old-store new-store)))
-      result))
+    (describe-and-swap-control-return!
+     (:management this)
+     (fn [old-store]
+       (let [[updated-store result] (update-fn old-store)
+             [new-store modified-ids] (fetch-and-clear-modified-ids
+                                       updated-store)]
+         [new-store
+          (keys-affected-by-ids modified-ids old-store new-store)
+          result]))))
 
   (add-simple-element! [this subject content]
     (do-update-control-return! this #(add-simple-element % subject content)))
@@ -173,6 +95,4 @@
 
 (defmethod new-mutable-store true [immutable-store]
   (map->MutableStoreImpl
-   {:data (atom {:state (track-modified-ids immutable-store)
-                 :subscriptions {}
-                 :fetch->attended-reporter {}})}))
+   {:management (new-mutable-management (track-modified-ids immutable-store))}))

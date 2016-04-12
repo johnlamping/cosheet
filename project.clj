adfb93fb1cc6eab5d6e4a7e59251b67155dec361
John Lamping <johnlamping@Johns-MacBook-Pro.local>
2016-04-11 22:48:53 -0700

    Move toward more recent dependencies

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"] ; 1.8.0
                  [org.clojure/data.priority-map "0.0.5"]
                  ;; Server side
                  [ring/ring-core "1.3.2"]
                  [ring/ring-jetty-adapter "1.3.2"]
                  [ring-transit "0.1.3"]
-                 [compojure "1.3.2"]
+                 [compojure "1.3.4"]
                  [hiccup "1.0.5"]
                  ;; Client side
-                 [org.clojure/clojurescript "0.0-2913"]
+                 [org.clojure/clojurescript "0.0-2913"] ; 3211; 1.8.40
                  [reagent "0.5.0-alpha3"]
                  [cljs-ajax "0.3.10"]
-                 [com.keminglabs/cljx "0.6.0"]
+                 [com.keminglabs/cljx "0.6.0"] ;; get rid of this.
                  ]
-  :plugins [[lein-cljsbuild "1.0.5"]
+  :plugins [[lein-cljsbuild "1.0.5"] ; 1.1.3
             [lein-ring "0.9.2"]
             [cider/cider-nrepl "0.10.0"]]
   :profiles {:uberjar {:aot :all}

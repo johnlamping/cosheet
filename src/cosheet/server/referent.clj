09be2141f0de76fd5551a6dcbc7b7d05ea9c3a11
John Lamping <johnlamping@Johns-MacBook-Pro.local>
2016-05-28 20:24:41 -0700

    Add referent.clj, to replace key.clj

---
+++ src/cosheet/server/referent.clj
@@ -0,0 +1,274 @@
+(ns cosheet.server.referent
+  (:require (cosheet [utils :refer [multiset]]
+                     [expression :refer [expr expr-let expr-seq]]
+                     [entity :as entity :refer [call-with-immutable
+                                                StoredEntity]]
+                     [store  :as store :refer [id-valid? StoredItemDescription]]
+                     [query :refer [matching-elements matching-items
+                                    template-matches]])))
+
+;;; Commands are typically run with respect to a referent, which
+;;; describes what the command should act on.
+
+;;; The semantics of a referent is, in general, a sequence of groups
+;;; of items. There are common special cases where the sequence has
+;;; only one member or the groups are singletons.
+
+;;; When a referent contains another referent as a
+;;; sequence-referent, it coerces that that referent into a sequence
+;;; of individual items, by concatenating the items in each group
+;;; together to for a single sequence.
+
+;;; When a referent contains a <condition>, it can be either an item
+;;; id or the list form of an element.
+
+;;; The kinds of referent are:
+;;;        item: <an item-id>
+;;;              Refers to the single item.
+;;;    exemplar: [:exemplar <item-id> <sequence-referent>]
+;;;              For each item refered to by referent, refers to an
+;;;              element of it whose semantic information is the same
+;;;              as that of the exemplar.
+;;;    elements: [:elements <condition> <sequence-referent>]
+;;;              For each item refered to by referent, refers to the
+;;;              group of elements of it whose semantic information
+;;;              matches the condition.
+;;;       query: [:query <condition>]
+;;;              Refers to the group of items that satisfies the condition.
+;;;       union: [:union <referent> ...]
+;;;              Refers to a group of items. Each referent must
+;;;              produce the same number of groups, and the union
+;;;              refers to the sequence of groups formed by unioning
+;;;              corresponding groups from the sequences.
+;;;  difference: [:difference <referent> <referent>]
+;;;              The two referents must produce the same number of
+;;;              groups of items. Refers to the sequence of
+;;;              differences of those groups.
+
+(defn item-referent? [referent]
+  (satisfies? StoredItemDescription referent))
+
+(comment
+
+  (defn exemplar-referent? [referent]
+    (and (sequential? referent) (= ( first referent) :exemplar)))
+
+  (defn elements-referent? [referent]
+    (and (sequential? referent) (= ( first referent) :elements)))
+
+  (defn query-referent? [referent]
+    (and (sequential? referent) (= ( first referent) :query)))
+
+  (defn union-referent? [referent]
+    (and (sequential? referent) (= ( first referent) :union)))
+
+  (defn difference-referent? [referent]
+    (and (sequential? referent) (= ( first referent) :difference))))
+
+(defn referent? [referent]
+  (or (item-referent? referent)
+      (and (sequential? referent)
+           (#{:exemplar :elements :query :union :difference}
+            (first referent)))))
+
+(defn referent-type [referent]
+  (assert (referent? referent) referent)
+  (if (item-referent? referent) :item (first referent)))
+
+;;; In the following constructors, items may be used wherever
+;;; item ids are required.
+
+(defn- coerce-item-to-id
+  "Turn items into their ids."
+  [item]
+  (if (satisfies? StoredEntity item)
+    (:item-id item)
+    item))
+
+(defn- require-item-or-id
+  "Make sure the argument is an item or id, and coerce it to an id."
+  [item]
+  (let [id (coerce-item-to-id item)]
+    (assert (satisfies? StoredItemDescription id))
+    id))
+
+(defn item-referent
+  "Create an item referent from an item."
+  [item]
+  (require-item-or-id item))
+
+(defn exemplar-referent
+  "Create an elements referent."
+  [exemplar subject]
+  (assert (referent? subject))
+  [:exemplar (require-item-or-id exemplar) subject])
+
+(defn elements-referent
+  "Create an elements referent."
+  [condition subject]
+  (assert (referent? subject))
+  [:elements (coerce-item-to-id condition) subject])
+
+(defn query-referent
+  "Create an query referent."
+  [condition]
+  [:query (coerce-item-to-id condition)])
+
+(defn union-referent
+  "Create a union referent."
+  [& referents]
+  (for [referent referents]
+    (assert (referent? referent)))
+  (into [:union] referents))
+
+(defn difference-referent
+  "Create a difference referent."
+  [plus minus]
+  (assert (referent? plus))
+  (assert (referent? minus))
+  [:difference plus minus])
+
+(defn make-item-or-exemplar
+  "Make an item or an exemplar, as necessary, depending on the subject."
+  [item subject]
+  (if (or  (empty? subject) (item-referent? subject))
+    (item-referent item)
+    (exemplar-referent item subject)))
+
+;;; For purposes of comparing two entities, not all of their elements
+;;; matter. In particular, order information, or other information
+;;; about how to display the elements is considered irrelevant for
+;;; matching a condition. We call the elements that matter the semantic
+;;; elements. Non-semantic elements will always themselves have an
+;;; element with :non-semantic as content.
+
+(defn semantic-element?-R
+  "Return true if an element counts as semantic information for its subject.
+  (Doesn't have a :non-semantic element.)"
+  [entity]
+  (expr-let [elements (entity/elements entity)
+             element-contents (expr-seq map entity/content elements)]
+    (not-any? #(= % :non-semantic) element-contents)))
+
+(defn semantic-elements-R
+  "Return the elements of an entity that are semantic to it."
+  [entity]
+  (expr-let [elements (entity/elements entity)
+             non-semantic (entity/label->elements entity :non-semantic)]
+    (remove (set non-semantic) elements)))
+
+(defn immutable-semantic-to-list
+  "Given an immutable item, make a list representation of the
+  semantic information of the item."
+  [item]
+  (if (entity/atom? item)
+    (entity/content item)
+    (let [content (entity/content item)
+          elements (semantic-elements-R item)
+          content-semantic (immutable-semantic-to-list content)
+          element-semantics (map immutable-semantic-to-list elements)]
+      (if (empty? element-semantics)
+        content-semantic
+        (list* (into [content-semantic] element-semantics))))))
+
+(defn semantic-to-list-R
+  "Given an item, make a list representation of the
+  semantic information of the item."
+  [item]
+  (call-with-immutable item immutable-semantic-to-list))
+
+(defn canonicalize-list
+  "Given the list form of an entity, return a canonical representation of it."
+  ;; We record the elements as a map from element to multiplicities,
+  ;; so that we are not sensitive to the order of the elements.
+  ;; That is easier than sorting, because Clojure doesn't define
+  ;; a sort order between heterogenous types, like strings and ints.
+  [entity]
+  (if (sequential? entity)
+    [(canonicalize-list (first entity))
+     (multiset (map canonicalize-list (rest entity)))]
+    entity))
+
+(defn item->canonical-semantic
+  "Return the canonical form of the semantic information for the item.
+  Only works on immutable items."
+  [item]
+  (canonicalize-list (immutable-semantic-to-list item)))
+
+(defn replace-nones
+  "Replace any :none in the seq with nil"
+  [x]
+  (cond (sequential? x) (map replace-nones x)
+        (= x :none) nil
+        true x))
+
+(defn condition-to-list
+  "If the condition is an item id, return the list form of its semantics,
+   otherwise, return the condition, assuming it is already in
+   semantic list form."
+  [condition immutable-store]
+  (replace-nones
+   (if (satisfies? StoredItemDescription condition)
+     (when (id-valid? immutable-store condition)
+       (semantic-to-list-R (description->entity condition immutable-store)))
+     condition)))
+
+(defn best-matching-element
+  "Given the list form of semantic information and an item,
+  find an element of the item that matches the semantic information.
+  Return the best matching element in a list if there is one, otherwise
+  return nil.
+  Only works on immutable items."
+  [semantic-info subject]
+  (let [matches (matching-elements semantic-info subject)]
+    (when (not (empty? matches))
+      (if (> (count matches) 1)
+        ;; Prefer one with no extra semantic info.
+        (let [canonical-semantic (canonicalize-list semantic-info)
+              perfect-matches (filter #(= (item->canonical-semantic %)
+                                          canonical-semantic)
+                                      matches)]
+          [(first (if (empty? perfect-matches) matches perfect-matches))])
+        matches))))
+
+(def instantiate-referent)
+
+(defn map-over-instances
+  "Instantiate the referent and call the function on each resulting item,
+   collecting the results into a sequence of groups."
+  [fun referent immutable-store]
+  (remove empty?
+          (map fun
+               (apply concat (instantiate-referent referent immutable-store)))))
+
+(defn instantiate-referent
+  "Return the groups of items that the referent refers to."
+  [referent immutable-store]
+  (case (referent-type referent)
+    :item (when (id-valid? immutable-store referent)
+            [[(description->entity referent immutable-store)]])
+    :exemplar (let [[_ exemplar subject] referent
+                    template (condition-to-list exemplar immutable-store)]
+                (map-over-instances #(best-matching-element template %)
+                                    subject immutable-store))
+    :elements (let [[_ condition subject] referent
+                    template (condition-to-list condition immutable-store)]
+                (map-over-instances #(matching-elements template %)
+                                    subject immutable-store))
+    :query (let [[_ condition] referent
+                 template (condition-to-list condition immutable-store)]
+             [(matching-items condition immutable-store)])
+    :union (let [[_ & referents] referent
+                 groups (map #(instantiate-referent % immutable-store)
+                             referents)]
+             (apply map (fn [& groups] (apply concat groups)) groups))
+    :difference (let [[_ plus minus] referent]
+                  (map (fn [p m] (remove (set m) p))
+                       (instantiate-referent plus immutable-store)
+                       (instantiate-referent minus immutable-store)))))
+
+
+ 
+
+
+
---
+++ test/cosheet/server/referent_test.clj
@@ -0,0 +1,130 @@
+(ns cosheet.server.referent-test
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.data :refer [diff]]
+            [clojure.pprint :refer [pprint]]
+            (cosheet [orderable :as orderable]
+                     [entity :as entity  :refer [description->entity]]
+                     [store :refer [new-element-store]]
+                     [store-impl :refer [->ItemId]]
+                     [store-utils :refer [add-entity]]
+                     [query :refer [matching-elements]]
+                     [expression :refer [expr expr-let expr-seq]]
+                     [debug :refer [simplify-for-print]]
+                     [test-utils :refer [check any as-set let-mutated]])
+            (cosheet.server [referent :refer :all])
+            ; :reload
+            ))
+
+(def orderables (reduce (fn [os _]
+                          (vec (concat (pop os)
+                                       (orderable/split (peek os) :after))))
+                        [orderable/initial]
+                        (range 4)))
+(def o1 (nth orderables 0))
+(def o2 (nth orderables 1))
+(def o3 (nth orderables 2))
+(def o4 (nth orderables 3))
+(def joe-list `("Joe"
+                (~o2 :order :non-semantic)
+                ("male" (~o1 :order :non-semantic))
+                (39 (~o3 :order :non-semantic)
+                    ("age" ~'tag)
+                    ("doubtful" "confidence"))
+                ("married" (~o2 :order :non-semantic))
+                (45 (~o4 :order :non-semantic)
+                    ("age" ~'tag))))
+(def jane-list `("Jane" (~o1 :order :non-semantic)
+                 ("female" (~o2 :order :non-semantic))
+                 (45 (~o3 :order :non-semantic)
+                     ("age" ~'tag))))
+(def t1 (add-entity (new-element-store) nil joe-list))
+(def joe-id (second t1))
+(def t2 (add-entity (first t1) nil jane-list))
+(def store (first t2))
+(def jane-id (second t2))
+(def joe (description->entity joe-id store))
+(def joe-age (first (matching-elements 45 joe)))
+(def joe-bogus-age (first (matching-elements 39 joe)))
+(def joe-age-tag (first (matching-elements "age" joe-age)))
+(def jane (description->entity jane-id store))
+(def jane-age (first (matching-elements 45 jane)))
+(def jane-age-tag (first (matching-elements "age" jane-age)))
+
+(deftest make-item-or-exemplar-test
+  (is (= (make-item-or-exemplar joe nil)
+         (item-referent joe)))
+   (is (= (make-item-or-exemplar joe (query-referent '(:hi :there)))
+          (exemplar-referent joe (query-referent '(:hi :there))))))
+
+(deftest semantic-test
+  (let [semantic (let-mutated [him joe-list]
+                  (semantic-to-list-R him))]
+    (is (= (first semantic) "Joe"))
+    (is (= (set (map canonicalize-list (rest semantic)))
+           #{"male"
+             "married"
+             '(39 {["age" {tag 1}] 1
+                   ("doubtful" {"confidence" 1}) 1})
+             '(45 {["age" {tag 1}] 1})})))
+  (is (= (set (map canonicalize-list
+                   (let-mutated [him joe-list]
+                     (expr-seq map entity/to-list (semantic-elements-R him)))))
+         (set (map canonicalize-list (rest (rest joe-list))))))
+  (let [expected ["Joe" {"male" 1
+                         "married" 1
+                         [39 {["age" {'tag 1}] 1
+                              ["doubtful" {"confidence" 1}] 1}] 1
+                              [45 {["age" {'tag 1}] 1}] 1}]]
+    (is (= (item->canonical-semantic joe-list) expected)))
+  (let [joes `("x"
+              ("Joe" ("name" ~'tag) ("id" ~'tag) (~o1 :order :non-semantic))
+              ("Joe" ("name" ~'tag) (~o2 :order :non-semantic))) ]
+    (is (= (best-matching-element '("Joe" ("name" tag)) joes)
+           [(nth joes 2)]))
+    (is (= (best-matching-element '("Joe" ("name" tag)  ("id" tag)) joes)
+           [(nth joes 1)]))
+    (is (= (best-matching-element '("Joe" ("age" tag)) joes)
+           nil))))
+
+(deftest instantiate-referent-test
+  (is (= (instantiate-referent (item-referent joe) store) [[joe]]))
+  (is (= (instantiate-referent
+          (exemplar-referent joe-age (item-referent jane)) store)
+         [[jane-age]]))
+  (is (check (instantiate-referent
+              (elements-referent '(nil ("age" tag)) (item-referent joe)) store)
+             [(as-set [joe-age joe-bogus-age])]))
+  (is (check (instantiate-referent (query-referent '(nil (nil "age"))) store)
+             [(as-set [joe jane])]))
+  (is (check (instantiate-referent
+              (exemplar-referent joe-age
+                                 (query-referent '(nil (nil "age")))) store)
+             (as-set [[joe-age] [jane-age]])))
+  (is (check (instantiate-referent
+              (elements-referent '(nil "age")
+                                 (query-referent '(nil (nil "age")))) store)
+             (as-set [(as-set [joe-age joe-bogus-age])
+                      [jane-age]])))
+  (is (check (instantiate-referent
+              (union-referent
+               (item-referent joe-age)
+               (elements-referent '(nil "age") (item-referent jane))
+               (query-referent '(nil (nil "age")))) store)
+             [(as-set [joe-age jane-age joe jane])]))
+  (is (check (instantiate-referent
+              (exemplar-referent joe-age
+                                 (union-referent (item-referent joe)
+                                                 (item-referent jane))) store)
+             (as-set [[joe-age] [jane-age]])))
+  (is (check (instantiate-referent
+              (difference-referent (query-referent '(nil (nil "age")))
+                                   (item-referent joe)) store)
+             [[jane]]))
+  (is (check (instantiate-referent
+              (exemplar-referent
+               (item-referent joe-age-tag)
+               (union-referent (elements-referent 45 (item-referent joe))
+                               (elements-referent 45 (item-referent jane))))
+              store)
+             (as-set [[joe-age-tag] [jane-age-tag]]))))
+

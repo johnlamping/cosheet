537e92d6172e319f2b1490abfc91fb3ef1197415
John Lamping <johnlamping@Johns-MacBook-Pro.local>
2016-06-07 08:59:43 -0700

    Partial render2 passes basic tests.

--- test/cosheet/server/render2_test.clj
+++ test/cosheet/server/render2_test.clj
@@ -14,12 +14,13 @@
              [expression-manager-test :refer [check-propagation]]
              entity-impl
              [store :refer [new-element-store id->content id->subject
-                            current-store]]
+                            make-id current-store]]
              store-impl
              [store-utils :refer [add-entity]]
              mutable-store-impl
              [dom-utils :refer [dom-attributes]]
-             [test-utils :refer [check any as-set evals-to let-mutated]])
+             [test-utils :refer [check any as-set evals-to
+                                 let-mutated item->immutable]])
             (cosheet.server
              [referent :refer [item-referent union-referent difference-referent
                            query-referent elements-referent
@@ -60,3 +61,80 @@
                     (expr-seq map to-list
                               (condition-satisfiers-R test '(nil :a :a :b)))))
              [:a])))
+
+(def orderables (reduce (fn [os _]
+                          (vec (concat (pop os)
+                                       (orderable/split (peek os) :after))))
+                        [orderable/initial]
+                        (range 4)))
+(def o1 (nth orderables 0))
+(def o2 (nth orderables 1))
+(def o3 (nth orderables 2))
+(def o4 (nth orderables 3))
+(def unused-orderable (nth orderables 4))
+
+(deftest item-DOM-R-test
+  (let [root-id (make-id "root")
+        initial {:priority 0
+                 :narrow true
+                 :parent-key [:root]
+                 :subject-referent root-id}]
+    ;; Test a simple cell
+    (let [[dom fred] (let-mutated [fred "Fred"]
+                   (expr-let [dom (item-DOM-R fred [] initial)]
+                     [dom (item->immutable fred)]))]
+      (is (check dom
+                 [:div {:class "content-text editable item"
+                        :key [:root (:item-id fred)]
+                        :target {:item-referent (item-referent fred)}
+                        :commands {:set-content nil
+                                   :add-element nil
+                                   :add-sibling nil
+                                   :delete nil}}
+                  "Fred"])))
+    ;; Test a single element.
+    (let [[dom age] (let-mutated [age `(39 ("doubtful"
+                                            ("confidence" :tag)
+                                            (~o1 :order :non-semantic)))]
+                      (expr-let [dom (item-DOM-R age [] initial)]
+                        [dom age]))
+          doubtful (first (current-value (matching-elements "doubtful" age)))
+          confidence (first (current-value
+                             (matching-elements "confidence" doubtful)))
+          confidence-tag (first (current-value (entity/elements confidence)))
+          item-key [:root (:item-id age)]]
+      (is (check
+           dom
+           [:div {:class "item with-elements"
+                  :key item-key
+                  :target {:item-referent (item-referent age)}}
+            [:div {:class "content-text editable"
+                   :key (conj item-key :content)
+                   :target {:item-referent (item-referent age)}
+                   :commands {:set-content nil
+                              :add-element nil
+                              :add-sibling nil
+                              :delete nil}}
+             "39"]
+            [:div {:class "wrapped-element tags"}
+             [:component {:key (conj item-key (:item-id confidence))
+                          :class "tag"}
+              [item-DOM-R confidence nil
+               {:priority 1
+                :narrow true
+                :parent-key item-key
+                :subject-referent (item-referent doubtful)
+                :selectable-attributes {}
+                :template '(nil :tag)}]]
+             [:div {:class "indent-wrapper"}
+              [:component {:key (conj item-key (:item-id doubtful))}
+               [item-DOM-R doubtful [confidence]
+                {:priority 1
+                 :narrow true,
+                 :parent-key item-key
+                 :subject-referent (item-referent age)
+                 :selectable-attributes
+                 {:commands {:add-row nil}
+                  :row {:subject-referent (item-referent age)
+                        :adjacents-referent (item-referent doubtful)}}}]]]]]
+           )))))

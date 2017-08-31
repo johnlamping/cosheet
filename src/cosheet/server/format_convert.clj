38371e262ef64139cc12065193f74e0624a1f7a6
John Lamping <john@johnlamping.com>
2017-08-31 13:07:53 -0700

    add format-convert

---
+++ src/cosheet/server/format_convert.clj
@@ -0,0 +1,37 @@
+(ns cosheet.server.format-convert
+  (:require (cosheet [debug :refer [simplify-for-print]]             
+                     [entity :refer [to-list label->elements content]]
+                     [store-utils :refer [add-entity remove-entity-by-id]]
+                     [query :refer [matching-items]])
+            (cosheet.server
+             [model-utils :refer [specialize-template]])))
+
+(defn convert-from-0-to-1
+  "Convert a store from format 0 to format 1.
+  The only difference between the two is that in format 1, table columns
+  appear as non-semantic elements of the row condition, rather than as
+  elements of the table."
+  [store]
+  (let [tables (matching-items '(nil :table) store)]
+    (reduce
+     (fn [store table]
+       (let [condition (first (label->elements table :row-condition))
+             columns (label->elements table :column)]
+         (reduce
+          (fn [store column]
+            (first (add-entity (remove-entity-by-id store (:item-id column))
+                               (:item-id condition)
+                               (concat (to-list column)
+                                       '[(:non-semantic :non-semantic)]))))
+          store columns)))
+     (first (add-entity store nil '(1 :format)))
+     tables)))
+
+(defn convert-to-current
+  "Convert a store to the current format (That's 1 right now."
+  [store]
+  (let [format (first (matching-items '(nil :format) store))
+        format (when format (content format))]
+    (cond (nil? format) (convert-from-0-to-1 store)
+          (= format 1) store
+          true (assert false (str ("Store in unknown format " format))))))
---
+++ test/cosheet/server/format_convert_test.clj
@@ -0,0 +1,66 @@
+(ns cosheet.server.format-convert-test
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.pprint :refer [pprint]]
+            (cosheet
+             [orderable :as orderable]
+             [entity  :refer [to-list]]
+             [canonical :refer [canonicalize-list]]
+             [store :refer [new-element-store]]
+             store-impl
+             [store-utils :refer [add-entity]]
+             [query :refer [matching-items]]
+             [debug :refer [simplify-for-print]]
+             entity-impl
+             [test-utils :refer [check any as-set evals-to let-mutated]])
+            (cosheet.server
+             [format-convert :refer :all])
+             ; :reload
+            ))
+
+(def orderables (reduce (fn [os _]
+                          (vec (concat (pop os)
+                                       (orderable/split (peek os) :after))))
+                        [orderable/initial]
+                        (range 6)))
+(def o1 (nth orderables 0))
+(def o2 (nth orderables 1))
+(def o3 (nth orderables 2))
+(def o4 (nth orderables 3))
+(def o5 (nth orderables 4))
+(def o6 (nth orderables 5))
+
+
+(deftest convert-test
+  (let [table-list-0 `("table"
+                       :table
+                       (~'anything (~'anything ("age" :tag))
+                        (:row-condition :non-semantic))
+                       (~'anything ("single" :tag (~o1 :order :non-semantic))
+                        (~o1 :order :non-semantic)
+                        (:column :non-semantic))
+                       (~'anything ("name" :tag (~o1 :order :non-semantic))
+                        (~o2 :order :non-semantic)
+                        (:column :non-semantic)))
+        table-list-1 `("table"
+                       :table
+                       (~'anything (~'anything ("age" :tag))
+                        (:row-condition :non-semantic)
+                        (~'anything ("single" :tag (~o1 :order :non-semantic))
+                         (~o1 :order :non-semantic)
+                         (:column :non-semantic)
+                         (:non-semantic :non-semantic))
+                        (~'anything ("name" :tag (~o1 :order :non-semantic))
+                         (~o2 :order :non-semantic)
+                         (:column :non-semantic)
+                         (:non-semantic :non-semantic))))]
+    (let [store-0 (first (add-entity (new-element-store) nil table-list-0))
+          store-1 (convert-from-0-to-1 store-0)
+          tables (matching-items '(nil :table) store-1)
+          versions (matching-items '(nil :format) store-1)]
+      (is (= (count tables) 1))
+      (is (check (canonicalize-list (to-list (first tables)))
+                 (canonicalize-list table-list-1)))
+      (is (= (count versions) 1))
+      (is (= (to-list (first versions)) '(1 :format)))
+      (is (= (convert-to-current store-0) store-1))
+      (is (= (convert-to-current store-1) store-1)))))

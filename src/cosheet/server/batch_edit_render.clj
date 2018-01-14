d69f4281c8822c349dba14f04e5f4c2017cb17a4
John Lamping <john@johnlamping.com>
2018-01-13 17:24:02 -0800

    Clean up

--- src/cosheet/server/batch_edit_render.clj
+++ src/cosheet/server/batch_edit_render.clj
@@ -103,81 +103,74 @@
 (defn batch-edit-stack-DOM-R
   "Return the dom for the edit stack part of the batch edit display."
   [query-item query-elements selected-batch-item store inherited]
-  (expr-let
-      [doms
-       (when selected-batch-item
-         (let [selected-referent (selected-batch-referent
-                                  selected-batch-item query-item
-                                  (query-items-referent query-item))
-               selected-non-header-referent
-               (selected-batch-referent
-                selected-batch-item query-item
-                (top-level-items-referent query-item))
-               inherited-for-batch
-               (-> inherited
-                   (assoc :selector-category :batch-stack)
-                   ;; Make its doms have different keys.
-                   (update :key-prefix #(conj % :batch-stack)))]
-           (expr-let
-               [virtual-dom (batch-edit-stack-virtual-DOM-R
-                             query-item query-elements inherited-for-batch)
-                batch-dom
-                (if (= query-item selected-batch-item)
-                  (let [query-and-rows-referent
-                        (union-referent [(item-referent query-item)
-                                         (top-level-items-referent query-item)])
-                        conditions-referent
-                        (table-conditions-referent query-item)
-                        inherited
-                        (->
-                         inherited-for-batch
-                         ;; We use a function for the subject, so that
-                         ;; all column headers will be changed, even if
-                         ;; there are several matching headers in one table.
-                         (assoc
-                          :subject-referent
-                          (fn
-                            ([]
-                             (union-referent [query-and-rows-referent
-                                              conditions-referent]))
-                            ([item]
-                             (let [item-ref (item-referent item)]
-                               (parallel-union-referent
-                                [(exemplar-referent
-                                  item-ref query-and-rows-referent)
-                                 (elements-referent
-                                  item-ref conditions-referent)])))))
-                         ;; If the selected item is the whole query, then a
-                         ;; delete could remove the whole condition of a
-                         ;; table. Have it just remove top level rows, instead.
-                         (add-inherited-attribute
-                          [#{:content}
-                           {:delete
-                            {:referent selected-non-header-referent}}]))]
-                    ;; TODO: Add-twin is a problem here with not knowing
-                    ;; what template to use.
-                    (labels-and-elements-DOM-R
-                     query-elements virtual-dom
-                     true true :horizontal inherited))
-                  (item-DOM-R
-                   selected-batch-item nil inherited-for-batch
-                   :referent selected-referent
-                   :must-show-label true))
-                count-dom
-                (call-dependent-on-id
-                 store nil
-                 (fn [store]
-                   (let [total (count (instantiate-to-items
-                                       selected-referent store))
-                         non-header (count (instantiate-to-items
-                                            selected-non-header-referent
-                                            store))]
-                     [:div {:class "batch-query-match-counts"}
-                      (str non-header " data matches. "
-                           (- total non-header 1) " header matches.")])))]
-             [count-dom
-              (add-attributes batch-dom {:class "batch-selected"})])))]
-    (into [:div {:class "batch-stack-wrapper"}] doms)))
+  (when selected-batch-item
+    (let [selected-referent (selected-batch-referent
+                             selected-batch-item query-item
+                             (query-items-referent query-item))
+          selected-non-header-referent
+          (selected-batch-referent
+           selected-batch-item query-item
+           (top-level-items-referent query-item))
+          inherited-for-batch
+          (-> inherited
+              (assoc :selector-category :batch-stack)
+              ;; Make its doms have different keys.
+              (update :key-prefix #(conj % :batch-stack)))]
+      (expr-let
+          [virtual-dom (batch-edit-stack-virtual-DOM-R
+                        query-item query-elements inherited-for-batch)
+           batch-dom
+           (if (= query-item selected-batch-item)
+             (let [query-and-rows-referent
+                   (union-referent [(item-referent query-item)
+                                    (top-level-items-referent query-item)])
+                   conditions-referent
+                   (table-conditions-referent query-item)
+                   inherited
+                   (-> inherited-for-batch
+                       ;; We use a function for the subject, so that
+                       ;; all column headers will be changed, even if
+                       ;; there are several matching headers in one table.
+                       (assoc
+                        :subject-referent
+                        (fn
+                          ([] (union-referent [query-and-rows-referent
+                                               conditions-referent]))
+                          ([item]
+                           (let [item-ref (item-referent item)]
+                             (parallel-union-referent
+                              [(exemplar-referent
+                                item-ref query-and-rows-referent)
+                               (elements-referent
+                                item-ref conditions-referent)])))))
+                       ;; If the selected item is the whole query, then a
+                       ;; delete could remove the whole condition of a
+                       ;; table. Have it just remove top level rows, instead.
+                       (add-inherited-attribute
+                        [#{:content}
+                         {:delete
+                          {:referent selected-non-header-referent}}]))]
+               ;; TODO: Add-twin is a problem here with not knowing
+               ;; what template to use.
+               (labels-and-elements-DOM-R
+                query-elements virtual-dom true true :horizontal inherited))
+             (item-DOM-R
+              selected-batch-item nil inherited-for-batch
+              :referent selected-referent :must-show-label true))
+           count-dom
+           (call-dependent-on-id
+            store nil
+            (fn [store]
+              (let [total (count (instantiate-to-items
+                                  selected-referent store))
+                    non-header (count (instantiate-to-items
+                                       selected-non-header-referent
+                                       store))]
+                [:div {:class "batch-query-match-counts"}
+                 (str non-header " data matches. "
+                      (- total non-header 1) " header matches.")])))]
+        [count-dom
+         (add-attributes batch-dom {:class "batch-selected"})]))))
 
 (defn batch-edit-DOM-R
   "Return the DOM for batch editing, given the item specifying the query,
@@ -210,4 +203,4 @@
               [:img {:src "../icons/exit.gif"}]
         [:div.tooltip "exit batch edit (C-Q)"]]
        (add-attributes query-dom {:class "batch-query"})
-       stack-dom])))
+       (into [:div {:class "batch-stack-wrapper"}] stack-dom)])))

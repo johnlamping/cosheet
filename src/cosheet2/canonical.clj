9441e5f4283c47980b30ca370ceff0f576d404f0
John Lamping <john@johnlamping.com>
2020-07-18 10:31:33 -0700

    Add canonical

---
+++ src/cosheet2/canonical.clj
@@ -0,0 +1,151 @@
+(ns cosheet2.canonical
+  (:require (cosheet2 [utils :refer [multiset multiset-diff multiset-sum
+                                     multiset-conj
+                                     canonical-atom-form equivalent-atoms?]])))
+
+;;; Utilities for converting to and from a canonical description
+;;; of an entity and the list form of it, and for operating on the
+;;; canonical description.
+
+;;; The canonical form describes atomic elements as the canonical form of
+;;; themselves, and non-atomic elements as a pair of the canonical form
+;;; of their content and a multiset of the canonical descriptions of
+;;; their elements.
+;;; This makes the description independent of the order of the elements
+;;; and the case of their strings.
+;;; It gets order independence easier than sorting, because Clojure doesn't
+;;; define a sort order between heterogenous types, like strings and ints.
+
+(defn canonicalize-list
+  "Given the list form of an entity, return a canonical representation of it."
+  [entity]
+  (if (sequential? entity)
+    [(canonicalize-list (canonical-atom-form (first entity)))
+     (multiset (map canonicalize-list (rest entity)))]
+    (canonical-atom-form entity)))
+
+(def canonical-to-list)
+
+(defn canonical-set-to-list
+  "Given a multiset of canonicalized lists or sets,
+   with the set also in canonical form, return a list of the items."
+  [set]
+  (when (not (empty? set))
+    (reduce (fn [result [key count]]
+              (concat result
+                      (repeat count (if (map? key)
+                                      (canonical-set-to-list key)
+                                      (canonical-to-list key)))))
+            [] (seq set))))
+
+(defn canonical-to-list
+  "Given a canonicalized list form of an item, return a list form for it."
+  [item]
+  (if (sequential? item)
+    (do (assert (= (count item) 2))
+        (cons (canonical-to-list (first item))
+              (canonical-set-to-list (second item))))
+    item))
+
+(def common-canonical)
+
+(defn canonical-content
+  "Return the content of a canonical representation."
+  [r]
+  (if (sequential? r) (first r) r))
+
+(defn update-canonical-content
+  "Update the content of a canonical representation."
+  [r c]
+  (let [ac (canonical-atom-form c)]
+    (if (sequential? r) [ac (second r)] ac)))
+
+(defn common-canonical-multisets-for-same-content
+  "Given two non-empty multisets of canonical representations,
+  all with the same content, return the multiset of representations
+  with the most in common."
+  ;;; TODO: We don't handle all cases of partial matching. The challenge is
+  ;;; that there isn't always a unique most in common, and this function
+  ;;; must be commutative. So we currently only handle identity, and when the
+  ;;; non identical stuff has only one item in each multiset. That should
+  ;;; cover the vast majority of cases, but it could do better. For example,
+  ;;; if one of the non-identical has only one item, and it has a largest
+  ;;; match with the other non-identical, that is still unique. (Largest
+  ;;; requires a notion of size of an item, which can be the number of
+  ;;; parts it has, which can be easily computed from its multiset.
+  [s1 s2]
+  (let [[first-only second-only both] (multiset-diff s1 s2)]
+    (or (when (and (= (count first-only) 1) (= (count second-only) 1))
+          (let [[first-item first-count] (first (seq first-only))
+                [second-item second-count] (first (seq second-only))
+                common (common-canonical first-item second-item)]
+            (when common
+              (let [count (min first-count second-count)]
+                (multiset-conj both common count)))))
+        (when (not (empty? both)) both)
+        (multiset-conj {} (canonical-content (first (keys s1)))))))
+
+(defn map-by-content
+  "Given a multiset of canonical representations, partition by content,
+  and return a map from content to multiset of representations with
+  that content."
+  [m]
+  (reduce
+   (fn [accum [item count]]
+     (update accum (canonical-content item)
+             #(multiset-conj (or % {}) item count)))
+   {} (seq m)))
+
+(defn common-canonical-multisets
+  "Given two multisets of canonical representations, return the multiset
+  of representations with the most in common."
+  [s1 s2]
+  (let [m1 (map-by-content s1)
+        m2 (map-by-content s2)
+        commons (keep (fn [[content1 items1]]
+                        (when-let [items2 (m2 content1)]
+                          (common-canonical-multisets-for-same-content
+                           items1 items2)))
+                      (seq m1))]
+    (reduce multiset-sum {} commons)))
+
+(defn common-canonical
+  "Given two canonical representations, return the canonincal
+  representation, if any, that captures their commmonality. To have
+  commonality, the contents must be the same. If that is satisfied,
+  then commonality is an item with that content. In addition, if
+  some of the elements of the first representation can be paired up
+  with elements of the second, and the pairs have commonality, then
+  those commonalities are elements of the overall commonality."
+  [c1 c2]
+  (let [content1 (canonical-content c1)
+        content2 (canonical-content c2)]
+    (when (= content1 content2)
+      (if (and (sequential? c1) (sequential? c2))
+        (let [common-elements (common-canonical-multisets
+                               (second c1) (second c2))]
+          (if (empty? common-elements)
+            content1
+            (list content1 common-elements)))
+        content1))))
+
+(defn canonical-extended-by
+  "Return true if every part of c1 has a corresponding part in c2.
+   Doesn't currently recognize all cases, where an element of the
+   first list corrresponds to an extension of it in the second."
+  [c1 c2]
+  (let [content1 (canonical-content c1)
+        content2 (canonical-content c2)]
+    (when (or (nil? content1) (= content1 content2))
+      (or (not (sequential? c1))
+          (and (sequential? c2)
+               (let [common-elements (common-canonical-multisets
+                                      (second c1) (second c2))]
+                 (= common-elements (second c1))))))))
+
+(defn canonical-have-common-elaboration
+  "Return true if there is a common elaboration of c1 and c2."
+  [c1 c2]
+  (let [content1 (canonical-content c1)
+        content2 (canonical-content c2)]
+    (or (nil? content1) (nil? content2) (= content1 content2))))
---
+++ test/cosheet2/canonical_test.clj
@@ -0,0 +1,92 @@
+(ns cosheet2.canonical-test
+  (:require [clojure.test :refer [deftest is]]
+            (cosheet2 [canonical :refer :all]
+                      [test-utils :refer [check]])
+            ; :reload
+            ))
+
+(def jane-list `("Jane" "plain" "plain"))
+(def joe-list '("Joe"
+                "Male"
+                (39 ("age" tag) ("doubtful" "confidence") )
+                "married"
+                (45 ("age" tag))))
+
+(deftest canonicalize-list-test
+  (is (check (canonicalize-list joe-list)
+             '("joe" {"male" 1
+                      "married" 1
+                      (39 {["age" {tag 1}] 1
+                           ("doubtful" {"confidence" 1}) 1}) 1
+                      (45 {["age" {tag 1}] 1}) 1}))))
+
+(deftest canonical-to-list-test
+  (let [starting [joe-list jane-list jane-list]
+        canonical (canonicalize-list [starting])]
+    (is (check (canonicalize-list (canonical-to-list canonical))
+               canonical))))
+
+(deftest update-canonical-content-test
+  (is (= (update-canonical-content (canonicalize-list jane-list) "Jeanette")
+         (canonicalize-list `("Jeanette" "plain" "plain"))))
+  (is (= (update-canonical-content (canonicalize-list "Jane") "Jeanette")
+         (canonicalize-list "Jeanette"))))
+
+(deftest common-canonical-test
+  (is (= (common-canonical "joe" "joe") "joe"))
+  (is (= (common-canonical "joe" "jane") nil))
+  (is (= (common-canonical "joe" (canonicalize-list '("joe" "name")))
+         "joe"))
+  (is (= (common-canonical "joe" (canonicalize-list '("jane" "name")))
+         nil))
+  (is (= (common-canonical (canonicalize-list '("joe" "appelation"))
+                           (canonicalize-list '("joe" "name")))
+         "joe"))
+  (is (= (common-canonical (canonicalize-list '("joe" "a" "name"))
+                           (canonicalize-list '("joe" "name" "b")))
+         (canonicalize-list '("joe" "name"))))
+  (is (= (common-canonical (canonicalize-list '("joe" "a" ("name" "c")))
+                           (canonicalize-list '("joe" "name" "b")))
+         (canonicalize-list '("joe" "name"))))
+  (is (= (common-canonical (canonicalize-list '("joe" "a" ("name" "c" "e")))
+                           (canonicalize-list '("joe" ("name" "d" "c") "b")))
+         (canonicalize-list '("joe" ("name" "c")))))
+  (is (= (common-canonical
+          (canonicalize-list '("joe" "a" "b" ("name" "c" "e")))
+          (canonicalize-list '("joe" ("name" "d" "c") "b" "b")))
+         (canonicalize-list '("joe" "b" ("name" "c")))))
+  (is (= (common-canonical
+          (canonicalize-list '("joe" "a" "b" ("name" "c" "e") "name"))
+          (canonicalize-list '("joe" ("name" "d" "c") "b" "b")))
+         (canonicalize-list '("joe" "b" "name"))))
+  (is (= (common-canonical
+          (canonicalize-list '("joe" "a" "b" ("name" "c" "e") "name"))
+          (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b")))
+         (canonicalize-list '("joe" "b" ("name" "e" "c") "name"))))
+  (is (= (common-canonical
+          (canonicalize-list '("joe" "a" "b" ("name" "c" "e") ("name" "c")))
+          (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b")))
+         (canonicalize-list '("joe" "b" ("name" "e" "c") ("name" "c"))))))
+
+(deftest canonical-extended-by-test
+  (is (canonical-extended-by
+       (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b"))
+       (canonicalize-list '("joe" "b" ("name" "c" "e") ("name" "c")))))
+  (is (canonical-extended-by
+       (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b"))
+       (canonicalize-list '("joe" "a" "b" ("name" "c" "e") ("name" "c")))))
+  (is (not (canonical-extended-by
+            (canonicalize-list '("joe" "a" "b" ("name" "c" "e") ("name" "c")))
+            (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b")))))
+  (is (not (canonical-extended-by
+            (canonicalize-list '("joe" "a" ("name" "c" "e") ("name" "c")))
+            (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b")))))
+  (is (not (canonical-extended-by
+            (canonicalize-list '("joe" "a" ("name" "c" "e") ("name" "c")))
+            (canonicalize-list '("joe" ("name" "e" "c") ("name" "c") "b"))))))
+
+(deftest canonical-have-common-elaboration-test
+  (is (canonical-have-common-elaboration '("joe" "a") '("joe" "b")))
+  (is (canonical-have-common-elaboration '(nil "a") '("joe" "b")))
+  (is (canonical-have-common-elaboration '("joe" "a") '(nil "b")))
+  (is (not (canonical-have-common-elaboration '("joe" "a") '("fred" "a")))))

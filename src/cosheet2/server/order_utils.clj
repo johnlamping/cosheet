afc56d7b80a2a8352a8cbb148e44492b0e10d574
John Lamping <john@johnlamping.com>
2020-08-15 20:54:59 -0700

    Add order-utils

---
+++ src/cosheet2/server/order_utils.clj
@@ -0,0 +1,175 @@
+(ns cosheet2.server.order-utils
+  (:require
+   (cosheet2
+    [orderable :refer [split earlier? initial]]
+    [store :refer [update-content add-simple-item declare-temporary-id]]
+    [entity :refer [content elements label->elements label->content]]
+    [query :refer [matching-items]]
+    [store-utils :refer [add-entity]]
+    [expression :refer [expr-let expr-seq]]
+    [utils :refer [thread-map]])))
+
+;;; This is copied from model_utils to avoid a circular dependency
+(defn semantic-element?
+  "Return true if an element counts as semantic information."
+  [immutable-entity]
+   (let [cont (content immutable-entity)]
+     (or (string? cont)
+         (number? cont)
+         (#{:label :category 'anything 'anything-immutable} cont))))
+
+(defn orderable-comparator
+  "Compare two sequences each of whose first element is an orderable."
+  [a b]
+  (earlier? (first a) (first b)))
+
+(defn order-items
+  "Return the immutable items in the proper sort order."
+  [items]
+  (if (empty? (rest items))
+    items
+    (let [order-info (map #(label->content % :order) items)]
+      (map second
+           (sort orderable-comparator
+                 (map (fn [order item]
+                        ;; It is possible for an item not to have
+                        ;; order information, especially temporarily
+                        ;; while information is being
+                        ;; propagated. Tolerate that.
+                        (vector (or order initial) item))
+                      order-info items))))))
+
+;;; TODO: Make a reporter version of order-items that takes a mutable
+;;; set of ids and a mutable store. It keeps a cache of the current
+;;; order value for each item, and registers for the elements that
+;;; hold those orders in the store. When the set of items changes, it
+;;; adds the order info for any new items to its cache, and when any
+;;; order info changes, it also updates the cache.
+
+(defn orderable-entity?
+  "Return whether this entity should get an order position."
+  [entity]
+  (and (semantic-element? entity) (not (keyword? entity))))
+
+(defn update-add-entity-with-order-and-temporary
+  "Add an entity, described in list form, to the store, with the given
+  subject.  Add ordering information to each part of the entity,
+  except for :label or :category specifiers and non-semantic elements,
+  splitting the provided order for the orders, and returning an unused
+  piece of it.  Put the new entity in the specified position (:before
+  or :after) of the returned order, and make the entity use the bigger
+  piece if use-bigger is true, otherwise return the bigger piece.  If
+  the entity has a :temporary element, mark it temporary in the store.
+  Return the new store, the id of the item, and the remaining order."
+  [store subject-id entity order position use-bigger]
+  (let [entity-content (content entity)
+        entity-elements (elements entity)
+        trans (some (fn [element] (= (content element) :temporary))
+                    entity-elements)]
+    (if (not (orderable-entity? entity))
+      (let [[s1 id] (add-entity store subject-id entity)]
+        [s1 id order])
+      (let [value-to-store entity-content
+            [s1 id] (add-simple-item store subject-id value-to-store)
+            ;; The next bunch of complication is to split the order up
+            ;; the right way in all cases. First, we split it into a
+            ;; bigger and a smaller part, putting the bigger part in
+            ;; correct position. Then, when we recursively add the
+            ;; elements, we take their order from the bigger position,
+            ;; leaving most of the space on the bigger position. Finally,
+            ;; we use the appropriate position for the entity and the
+            ;; return value.
+            entity-order-index (case position :before 0 :after 1)
+            other-position ([:after :before] entity-order-index)
+            split-order (split order (if use-bigger position other-position))
+            bigger-index (if use-bigger
+                           entity-order-index
+                           (- 1 entity-order-index))
+            bigger-order (split-order bigger-index)
+            smaller-order (split-order (- 1 bigger-index))
+            [s2 _ bigger-order]
+            (reduce (fn [[store _ order] element]
+                      (update-add-entity-with-order-and-temporary
+                       store id element order position false))
+                    [s1 nil bigger-order]
+                    (case position ;; Make the order match
+                      :before entity-elements
+                      :after (reverse entity-elements)))
+            [s3 _] (add-entity
+                    s2 id `(~(if use-bigger bigger-order smaller-order)
+                            :order))]
+        [(if trans (declare-temporary-id s3 id) s3)
+         id
+         (if use-bigger smaller-order bigger-order)]))))
+
+(defn furthest-item
+  "Given a list of items and a position,
+  return the furthest item in that position."
+  [items position]
+  (cond
+    (empty? items) nil
+    (= (count items) 1) (first items)
+    true (second
+          (reduce (case position
+                    :before (fn [a b] (if (earlier? (first a) (first b)) a b))
+                    :after (fn [a b] (if (earlier? (first a) (first b)) b a)))
+                  (map (fn [item] [(label->content item :order) item])
+                       items)))))
+
+(defn furthest-element [item position]
+  "Return the furthest element of the item, in the direction of the position.
+   If the item has no ordered elements, return the item."
+  (let [candidates (filter (fn [element] (label->content element :order))
+                           (elements item))]
+    (if candidates
+      (furthest-item candidates position)
+      item)))
+
+(defn order-element-for-item
+  "Return an element with the order information for item,
+   or, if that is not available, for the overall store."
+  [item store]
+  (or (first (label->elements item :order))
+      (first (matching-items '(nil :unused-orderable) store))))
+
+(defn update-add-entity-adjacent-to
+  "Add an entity with the given subject id and contents,
+   taking its order from the given item, in the given position,
+   and giving the entity the bigger piece if use-bigger is true.
+   Return the updated store and the id of the entity."
+  [store subject-id entity adjacent-to position use-bigger]
+  (let [order-element (order-element-for-item adjacent-to store)
+        order (content order-element)
+        [store id remainder] (update-add-entity-with-order-and-temporary
+                              store subject-id entity
+                              order position use-bigger)]
+    [(update-content store (:item-id order-element) remainder) id]))
+
+(defn add-order-elements-internal
+  "This form uses the specified order to order the elements,
+   and returns the new list and the unused part of order."
+  [entity order]
+  (cond
+    (sequential? entity)
+    (let [[elements order] (thread-map add-order-elements-internal
+                                       (rest entity) order)
+          [before after] (split order :after)]
+      [(apply list (concat [(first entity)]
+                           elements
+                           [`(~before :order)]))
+       after])
+    (orderable-entity? entity)
+    (let [[before after] (split order :after)]
+      [`(~entity (~before :order))
+       after])
+    true
+    [entity order]))
+
+(defn add-order-elements
+  "Given the list form of the semantic part of an item, add order
+  information to each user selectable part so they are in the same
+  order as in the list form. (If order information isn't added to a
+  new item, queries may fail to find it, as the presence of order
+  information is how queries restrict to semantic elements."
+  [entity]
+  (first (add-order-elements-internal entity initial)))
---
+++ test/cosheet2/server/order_utils_test.clj
@@ -0,0 +1,146 @@
+(ns cosheet2.server.order-utils-test
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.data :refer [diff]]
+            [clojure.pprint :refer [pprint]]
+            (cosheet2
+             [entity :as entity :refer [description->entity to-list
+                                        label->elements elements]]
+             [orderable :as orderable]
+             [debug :refer [profile-and-print-reporters
+                            simplify-for-print]]
+             entity-impl
+             [query :refer [matching-elements]]
+             [store :refer [new-element-store]]
+             store-impl
+             [store-utils :refer [add-entity]]
+             [canonical :refer [canonicalize-list]]
+             [test-utils :refer [check any]])
+            (cosheet2.server
+             [order-utils :refer :all])
+            ; :reload
+            ))
+
+(def orderables (reduce (fn [os _]
+                          (vec (concat (pop os)
+                                       (orderable/split (peek os) :after))))
+                        [orderable/initial]
+                        (range 4)))
+(def o1 (nth orderables 0))
+(def o2 (nth orderables 1))
+(def o3 (nth orderables 2))
+(def o4 (nth orderables 3))
+(def unused-orderable (nth orderables 4))
+(def joe-list `("Joe"
+                (~o2 :order)
+                ("male" (~o1 :order))
+                (39 (~o3 :order)
+                    ("age" :label)
+                    ("doubtful" "confidence"))
+                ("married" (~o2 :order))
+                (45 (~o4 :order)
+                    ("age" :label))))
+(def t1 (add-entity (new-element-store) nil joe-list))
+(def joe-id (second t1))
+(def store (first t1))
+(def joe (description->entity joe-id store))
+(def joe-male (first (matching-elements "male" joe)))
+(def joe-married (first (matching-elements "married" joe)))
+(def joe-45 (first (matching-elements 45 joe)))
+
+(deftest update-add-entity-with-order-test
+  (let [[s id order] (update-add-entity-with-order-and-temporary
+                      store joe-id 6
+                      unused-orderable :before true)
+        joe (description->entity joe-id s)
+        new-entity (first (matching-elements 6 joe))
+        [o5 o6] (orderable/split unused-orderable :before)]
+    (is (= (to-list new-entity)
+           `(6 (~o5 :order))))
+    (is (= order o6))
+    (is (= (:item-id new-entity) id)))
+  (let [[s id order] (update-add-entity-with-order-and-temporary
+                      store joe-id 6
+                      unused-orderable :before false)
+        joe (description->entity joe-id s)
+        new-entity (first (matching-elements 6 joe))
+        [o5 o6] (orderable/split unused-orderable :after)]
+    (is (= (to-list new-entity)
+           `(6 (~o5 :order))))
+    (is (= order o6))
+    (is (= (:item-id new-entity) id)))    
+  (let [[s id order] (update-add-entity-with-order-and-temporary
+                      store joe-id 6
+                      unused-orderable :after true)
+        joe (description->entity joe-id s)
+        new-entity (first (matching-elements 6 joe))
+        [o5 o6] (orderable/split unused-orderable :after)]
+    (is (= (to-list new-entity)
+           `(6 (~o6 :order))))
+    (is (= order o5))
+    (is (= (:item-id new-entity) id)))
+  (let [[s id order] (update-add-entity-with-order-and-temporary
+                      store joe-id '(6 ("height" :label))
+                      unused-orderable :before true)
+        joe (description->entity joe-id s)
+        new-entity (first (label->elements joe "height"))
+        [x o5] (orderable/split unused-orderable :before)
+        [o6 o7] (orderable/split x :after)]
+    (is (check (canonicalize-list (to-list new-entity))
+               (canonicalize-list `(6 (~o7 :order)
+                                      ("height" :label
+                                       (~o6 :order))))))
+    (is (= order o5))
+    (is (= (:item-id new-entity) id)))
+  ;; Check that order in the list style entity is preserved in the
+  ;; :order values.
+  ;; Also check and that non-semantic elements don't get order information
+  ;; and that the entity is marked temporary, if requested.
+  (let [[s id order] (update-add-entity-with-order-and-temporary
+                      store joe-id '(6 ("height" :label)
+                                       ("" :label)
+                                       :temporary
+                                       (:other ""))
+                      unused-orderable :after false)
+        joe (description->entity joe-id s)
+        new-entity (first (label->elements joe "height"))
+        [x o5] (orderable/split unused-orderable :before)
+        [x o6] (orderable/split x :before)
+        [o8 o7] (orderable/split x :before)]
+    (is (check (canonicalize-list (to-list new-entity))
+               (canonicalize-list
+                `(6 (~o5 :order)
+                    ("height" :label (~o7 :order))
+                    ("" :label (~o6 :order))
+                    :temporary
+                    (:other "")))))
+    (is ((:temporary-ids s) id))
+    (is (= order o8))
+    (is (= (:item-id new-entity) id))))
+
+(deftest furthest-item-test
+  (is (= (furthest-item [joe-married] :before) joe-married))
+  (is (= (furthest-item [joe-married joe-male] :before) joe-male))
+  (is (= (furthest-item [joe-married joe-male] :after) joe-married)))
+
+(deftest furthest-element-test
+  (is (= (furthest-element joe :after) joe-45))
+  (is (= (furthest-element joe :before) joe-male)))
+
+(deftest add-order-elements-test
+  (let [ordered (add-order-elements `("a" ("b" "c") "d" ("e" :label)))]
+    (is (check ordered
+               `("a" ("b" ("c" (~(any) :order))
+                      (~(any) :order))
+                   ("d" (~(any) :order))
+                   ("e" :label (~(any) :order))
+                   (~(any) :order))))
+    (is (orderable/earlier? (-> ordered second second second first)
+                            (-> ordered second (nth 2) first)))
+    (is (orderable/earlier? (-> ordered second (nth 2) first)
+                            (-> ordered (nth 2) second first)))
+    ;; The last element is not semantic, as it is order information.
+    (let [semantic-elements (butlast (elements ordered))]
+      (is (check (order-items semantic-elements)
+                 semantic-elements))
+      (is (check (order-items (reverse semantic-elements))
+                 semantic-elements)))))

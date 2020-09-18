6668099d778a9a32ce2a2e1648532eb219f7eeab
John Lamping <john@johnlamping.com>
2020-09-18 14:50:59 -0700

    Add map-state

---
+++ src/cosheet2/map_state.clj
@@ -0,0 +1,32 @@
+(ns cosheet2.map-state
+  (:require (cosheet2
+             [reporter :refer [new-reporter
+                               change-data! change-data-control-return!]]
+             [expression :refer [expr category-change]]
+             [category-change-calculator :refer [category-change-calculator]])))
+
+;;; Support a reporter that holds a map.
+
+(defn new-map-state
+  [initial]
+  (assert (map? initial))
+  (new-reporter :value initial))
+
+(defn map-state-get [map-state key]
+  (expr key (category-change [key] map-state)))
+
+(defn map-state-change-value! [map-state key fun]
+  (change-data! map-state
+                (fn [data]
+                  [(assoc-in data [:value key] (fun (key (:value data))))
+                   [key]
+                   [key]])))
+
+(defn map-state-change-value-control-return! [map-state key fun]
+  (change-data-control-return!
+   map-state
+   (fn [data] (let [[new-val result] (fun (key (:value data)))]
+                [(assoc-in data [:value key] new-val)
+                 [key]
+                 [key] result]))))
+
---
+++ test/cosheet2/map_state_test.clj
@@ -0,0 +1,66 @@
+(ns cosheet2.map-state-test
+  (:require [clojure.test :refer [deftest is]]
+            [clojure.pprint :refer [pprint]]
+            (cosheet2 [map-state :refer :all]
+                      [task-queue :refer [new-priority-task-queue]]
+                      [calculator :refer [new-calculator-data
+                                          compute
+                                          propagate-calculator-data!]]
+                      [reporter  :refer [new-reporter reporter-value 
+                                         reporter-data
+                                         valid? invalid
+                                         set-attendee!]]
+                      [test-utils :refer [check any]])
+            ; :reload
+            ))
+
+(deftest map-state-test
+  (let [cd (new-calculator-data (new-priority-task-queue 0))
+        ms (new-map-state {:a 1 :b 2})
+        ra (map-state-get ms :a)
+        rb (map-state-get ms :b)
+        rc (map-state-get ms :c)
+        history (atom [])
+        callback (fn [& {:keys [key reporter]}]
+                   (swap! history #(conj % [key (reporter-value reporter)])))]
+    (propagate-calculator-data! ra cd)
+    (propagate-calculator-data! rb cd)
+    (propagate-calculator-data! rc cd)
+    (is (not (valid? ra)))
+    (set-attendee! ra :ra 1 callback)
+    (is (= @history []))
+    (compute cd)
+    (is (= (reporter-value ra) 1))
+    (is (not (valid? rb)))
+    (is (check @history
+               [[:ra invalid]
+                [:ra 1]]))
+    (set-attendee! rc :rc 100 callback)
+    (compute cd)
+    (is (check @history
+               [[:ra invalid]
+                [:ra 1]
+                [:rc invalid]
+                [:rc nil]]))
+    (map-state-change-value! ms :a (fn [x] (+ x 9)))
+    (compute cd)
+    (is (check @history
+               [[:ra invalid]
+                [:ra 1]
+                [:rc invalid]
+                [:rc nil]
+                [:ra invalid]
+                [:ra 10]]))
+    (is (= (map-state-change-value-control-return!
+            ms :c (fn [x] [[x "hi"] "there"]))
+           "there"))
+    (compute cd)
+    (is (check @history
+               [[:ra invalid]
+                [:ra 1]
+                [:rc invalid]
+                [:rc nil]
+                [:ra invalid]
+                [:ra 10]
+                [:rc invalid]
+                [:rc [nil "hi"]]]))))
